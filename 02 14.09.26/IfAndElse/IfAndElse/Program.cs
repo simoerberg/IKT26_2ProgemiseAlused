3f42665114c3e6226037d8af60b18a863e11f2ef
simoerberg <simoerberg19@gmail.com>
2026-09-14 09:58:07 +0300

    IfAndElse

---
+++ 02 14.09.26/IfAndElse/IfAndElse/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace IfAndElse
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Sisesta enda nimi");
+
+            //siin on muutuja nimega name, mis on tüübiga string
+            //loeb andmeid konsoolist ja salvestab need muutuja name sisse
+            string name = Console.ReadLine();
+
+            //if ja else kontrollib, kas muutuja  name on tühi või mitte
+
+            //kui muutuja name on tühi, siis väljastab konsoolile
+            //teksti "Tere, tundmatu! ERROR" ja teeb 4 piiksu
+            if (name != "")
+            {
+                Console.BackgroundColor = ConsoleColor.Green;
+                Console.WriteLine("Tere, " + name);
+            }
+            else
+            {
+                Console.BackgroundColor = ConsoleColor.Red;
+                Console.WriteLine("Tere, tundmatu! ERROR");
+                Console.Beep();
+                Thread.Sleep(1000);
+                Console.Beep();
+                Thread.Sleep(1000);
+                Console.Beep();
+                Thread.Sleep(1000);
+                Console.Beep();
+                Thread.Sleep(1000);
+            }
+        }
+    }
+}
